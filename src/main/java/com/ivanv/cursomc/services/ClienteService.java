2284c138a8d5b914efd81a711ae9a677b6c141e1
Ivan Viana <ivan.viana@live.com>
2019-12-20 13:32:49 -0300

    Configura endpoint /clientes/{id}

---
+++ src/main/java/com/ivanv/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.ivanv.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ivanv.cursomc.domain.Cliente;
+import com.ivanv.cursomc.repositories.ClienteRepository;
+import com.ivanv.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
