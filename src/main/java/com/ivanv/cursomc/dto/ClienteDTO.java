1204162bb28fec5116161f1caa4930370a81cfe2
Ivan Viana <ivan.viana@live.com>
2019-12-25 20:00:54 -0300

    Valida atualização de cliente com email já existente

--- src/main/java/com/ivanv/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/ivanv/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.ivanv.cursomc.domain.Cliente;
+import com.ivanv.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
