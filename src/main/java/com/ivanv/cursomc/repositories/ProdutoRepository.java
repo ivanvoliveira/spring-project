ee6007f8fdd3a43ffd7f562eb93d9881be5de75a
Ivan Viana <ivan.viana@live.com>
2020-01-05 15:33:20 -0300

    Operações nas classes de dominio

--- src/main/java/com/ivanv/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/ivanv/cursomc/repositories/ProdutoRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ivanv.cursomc.domain.Categoria;
 import com.ivanv.cursomc.domain.Produto;
@@ -14,5 +15,6 @@ import com.ivanv.cursomc.domain.Produto;
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 	
 	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	@Transactional(readOnly = true)
 	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
