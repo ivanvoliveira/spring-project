4c15c4ee43c4a6af0af59527935a8aea6164a5f2
Ivan Viana <ivan.viana@live.com>
2020-01-06 20:11:48 -0300

    Adiciona senha a cliente e correção de campo email no domain

--- src/main/java/com/ivanv/cursomc/domain/Cliente.java
+++ src/main/java/com/ivanv/cursomc/domain/Cliente.java
@@ -26,12 +26,15 @@ public class Cliente implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	private String nome;
 	
 	@Column(unique = true)
-	private String nome;
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
+	
+	@JsonIgnore
+	private String senha;
 	
 	@OneToMany(mappedBy="cliente", cascade=CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
@@ -44,13 +47,14 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = (tipo==null) ? null :tipo.getCod();
+		this.senha = senha;
 	}
 	
 	public Cliente() {
@@ -94,6 +98,14 @@ public class Cliente implements Serializable {
 	
 	public void setTipo(TipoCliente tipo) {
 		this.tipo = tipo.getCod();
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 	
 	public List<Endereco> getEnderecos() {
@@ -144,4 +156,5 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
+
 }
