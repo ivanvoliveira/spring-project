f8367b89d15dcf2051b5896c6b4288f66d866d37
Ivan Viana <ivan.viana@live.com>
2020-01-07 17:17:12 -0200

    Redefinindo senha

--- src/main/java/com/ivanv/cursomc/services/EmailService.java
+++ src/main/java/com/ivanv/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.ivanv.cursomc.domain.Cliente;
 import com.ivanv.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -15,4 +16,6 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendHtmlEmail(MimeMessage msg);
+
+	void sendNewPasswordEmaill(Cliente cliente, String newPass);
 }
